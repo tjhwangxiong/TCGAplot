f50e349a2be7d83374a037b24c342d99dd1fa664
Xiong Wang <94770704+tjhwangxiong@users.noreply.github.com>
2023-11-21 12:06:43 +0800

    Delete R directory

--- R/get_methy.R
+++
@@ -1,8 +0,0 @@
-#' get_methy
-#'
-#' @description Show the download link of the whole methylation mtrix with 8Gb.
-#' @export
-#' @examples get_methy()
-get_methy=function () {
-  print("Please download the whole methylation mtrix with 8Gb from: https://drive.google.com/file/d/1UHEc_MlHBnsh794lxbJnV4l6oqKx5eso/view?usp=sharing")
-}
