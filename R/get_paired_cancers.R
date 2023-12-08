4662cfcdebbee04bb8de751f373f9762187c20fd
Xiong Wang <94770704+tjhwangxiong@users.noreply.github.com>
2023-12-08 22:41:53 +0800

    Delete R directory

--- R/get_paired_cancers.R
+++
@@ -1,9 +0,0 @@
-#' get_paired_cancers
-#'
-#' @description Return the sample summary of 15 types of cancer containing more than 20 paired samples in TCGA.
-#' @export
-#' @examples get_paired_cancers()
-get_paired_cancers=function(){
-  table(paired_tpm$Cancer,paired_tpm$Group)
-}
-
